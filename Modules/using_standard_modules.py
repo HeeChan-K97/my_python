4b2094074533ff8a771b80f8bf018ce554487d78
HeeChan Kwon <heechan6431@gmail.com>
2022-06-15 20:58:57 +1000

    15 June 2022

--- Modules/using_standard_modules.py
+++ Modules/using_standard_modules.py
@@ -1,7 +1,13 @@
+#the python standard library: docs.python.org, refer to chapter 6, sys module
 import sys
+import os
+# import datetime
 
 def main():
-    v = sys.version_info
+    v = sys.version_info#version_info: returns a collection of information
+    v = sys.platform
+    v = os.name
+    v = os.getenv('PATH')
     print('Python version {}.{}.{}'.format(*v))
 
 if __name__ == '__main__': main()
